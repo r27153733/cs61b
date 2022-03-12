afd111eb6b84a1ad6dc4ece3de4ce8cf9bd3b15a
r27153733 <r27153733@gmail.com>
2022-03-13 00:28:13 +0800

    poj1-ongakunohiro-EC-2

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -20,12 +20,12 @@ public class TestArrayDequeEC {
                 t.addFirst(i + actual1);
                 s.addFirst(i + actual1);
                 sb.append("addFirst(").append(i + actual1).append(")\n");
-                assertEquals(sb.toString(), t.get(0), s.get(0));
+//                assertEquals(sb.toString(), t.get(0), s.get(0));
             } else if (i % 3 == 0) {
                 t.addLast(i + actual1);
                 s.addLast(i + actual1);
                 sb.append("addLast(").append(i + actual1).append(")\n");
-                assertEquals(sb.toString(), t.get(t.size() - 1), s.get(s.size() - 1));
+//                assertEquals(sb.toString(), t.get(t.size() - 1), s.get(s.size() - 1));
             } else if (i % 2 == 0 && t.size() != 0) {
                 Integer integerT = t.removeFirst();
                 Integer integerS = s.removeFirst();
@@ -34,8 +34,9 @@ public class TestArrayDequeEC {
             } else if (i % 5 == 0 && t.size() != 0) {
                 Integer integerT = t.removeLast();
                 Integer integerS = s.removeLast();
-                assertEquals(sb.toString(), integerT, integerS);
                 sb.append("removeLast()\n");
+                assertEquals(sb.toString(), integerT, integerS);
+
             }
             actual1 = StdRandom.uniform(10);
         }
